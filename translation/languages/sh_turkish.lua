122121c52664cc50f47cea78180c1c3576a8a359
wilderwesten <wilderwesten64@gmail.com>
2025-12-03 16:39:38 +0300

    turkish

---
+++ translation/languages/sh_turkish.lua
@@ -0,0 +1,16 @@
+LANGUAGE = {
+    translationMenuTitle = "Dil Seçimi",
+    translationMenuSubtitle = "Sohbet çevirisi için tercih ettiğiniz dili seçin",
+    translationMenuSubtitleFirst = "Hoş geldiniz! Lütfen tercih ettiğiniz dili seçin.",
+    translationCurrentLanguage = "Mevcut dil",
+    translationConfirm = "ONAYLA",
+    translationCancel = "İPTAL",
+    cmdLanguage = "Dil seçim menüsünü açar.",
+    cmdSetLanguage = "Dili doğrudan ayarlar. Kullanım: /SetLanguage <language_code>",
+    translationLanguageSet = "Diliniz %s olarak ayarlandı.",
+    translationInvalidLanguage = "Geçersiz dil kodu. Kullanılabilir seçenekleri görmek için /Language komutunu kullanın.",
+    translationEnabled = "Çeviri sistemi etkinleştirildi.",
+    translationDisabled = "Çeviri sistemi kapatıldı.",
+    translationOriginalText = "Orijinal",
+    translationTranslated = "Çevrilmiş"
+}
